308a264ac32943360e6052a4a9b8c275659dcbd7
Eddie <eddiepykosh@gmail.com>
2021-03-22 23:00:10 -0400

    need lilypond and MIDIFile WIP

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 from midiutil.MidiFile import MIDIFile
+import os
 
+home = os.path.expanduser('~')
+print(home)
+docsHome = os.path.join(home, 'Documents')
+
+folderCheck = os.path.isdir(docsHome)
+if not (folderCheck):
+    print("You do not have a documents folder")
+    exit(-4)
+
+#def export()
 # create your MIDI object
 mf = MIDIFile(1)     # only 1 track
 track = 0   # the only track
@@ -14,7 +25,7 @@ volume = 100
 
 pitch = 60           # C4 (middle C)
 time = 0             # start on beat 0
-duration = 1         # 1 beat long
+duration = 0.5       # 1 beat long
 mf.addNote(track, channel, pitch, time, duration, volume)
 
 pitch = 64           # E4
@@ -24,9 +35,37 @@ mf.addNote(track, channel, pitch, time, duration, volume)
 
 pitch = 67           # G4
 time = 4             # start on beat 4
-duration = 1         # 1 beat long
+duration = 1        # 1 beat long
 mf.addNote(track, channel, pitch, time, duration, volume)
 
+pitch = 67           # G4
+time = 5             # start on beat 5
+duration = 1        # 1 beat long
+mf.addNote(track, channel, pitch, time, duration, volume)
+
+
 # write it to disk
-with open("output.mid", 'wb') as outf:
+rawName =input("Give a name for the file: ")
+fileName = rawName + ".mid"
+outFile = "processing/"+fileName
+with open(outFile, 'wb') as outf:
     mf.writeFile(outf)
+
+toLY = "cd LilyPond/usr/bin/ &&  py midi2ly --output="
+lyOut = '"C:/Users/Eddie/PycharmProjects/MidiCreator/processing" ' #need universal
+lyIN = '"' "C:/Users/Eddie/PycharmProjects/MidiCreator/processing/" + fileName + '"' #need universal
+
+alterName = rawName + "-midi.ly"
+
+toPDF = "cd LilyPond/usr/bin/ &&  lilypond --output="
+pdfOut = '"C:/Users/Eddie/PycharmProjects/MidiCreator/Final PDFs" ' #need universal
+pdfIN = '"' "C:/Users/Eddie/PycharmProjects/MidiCreator/processing/" + alterName + '"' #need universal
+
+stream = os.popen(toLY + lyOut +lyIN)
+output = stream.read()
+print (output)
+
+stream = os.popen(toPDF + pdfOut + pdfIN)
+output = stream.read()
+print (output)
+
